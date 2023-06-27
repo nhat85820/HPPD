49f3e5f77fb33c523bb7e9e02088dd1c3194a96d
nhat85820 <101626673+nhat85820@users.noreply.github.com>
2023-06-27 07:57:10 +0700

    Update index.js

--- HPBD/index.js
+++ HPBD/index.js
@@ -1,5 +1,5 @@
-const contentLetterSrart_actived = "Hãy nói gì đó ở đây trước khi người ấy mở bức thư nhé." //Lời mở đầu cho bức thư
-const mainContentLetter = "Gửi lời nhắn nhủ đến người bạn bạn yêu thương." //Nội dung của bức thư
+const contentLetterSrart_actived = "Chúc mừng sinh nhật cậu." //Lời mở đầu cho bức thư
+const mainContentLetter = "Sinh nhật vui vẻ nhé!." //Nội dung của bức thư
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
